2aadede4b523187d8f8ead1fa98cc04783632e2c
Peter Atashian <retep998@gmail.com>
2014-11-30 03:08:10 -0500

    Add more of playsoundapi.h
    Signed-off-by: Peter Atashian <retep998@gmail.com>

--- src/lib.rs
+++ src/lib.rs
@@ -1350,6 +1350,27 @@ pub struct IUnknown {
 }
 pub type LPUNKNOWN = *mut IUnknown;
 
+//-------------------------------------------------------------------------------------------------
+// playsoundapi.h
+// ApiSet Contract for api-ms-win-mm-playsound-l1-1-0
+//-------------------------------------------------------------------------------------------------
+pub const SND_SYNC: DWORD = 0x0000;
+pub const SND_ASYNC: DWORD = 0x0001;
+pub const SND_NODEFAULT: DWORD = 0x0002;
+pub const SND_MEMORY: DWORD = 0x0004;
+pub const SND_LOOP: DWORD = 0x0008;
+pub const SND_NOSTOP: DWORD = 0x0010;
+pub const SND_NOWAIT: DWORD = 0x00002000;
+pub const SND_ALIAS: DWORD = 0x00010000;
+pub const SND_ALIAS_ID: DWORD = 0x00110000;
+pub const SND_FILENAME: DWORD = 0x00020000;
+pub const SND_RESOURCE: DWORD = 0x00040004;
+pub const SND_PURGE: DWORD = 0x0040;
+pub const SND_APPLICATION: DWORD = 0x0080;
+pub const SND_SENTRY: DWORD = 0x00080000;
+pub const SND_RING: DWORD = 0x00100000;
+pub const SND_SYSTEM: DWORD = 0x00200000;
+
 // old stuff
 #[repr(C)]
 pub struct CONSOLE_READCONSOLE_CONTROL {
@@ -1851,5 +1872,13 @@ extern "system" {
         pszSound: LPCWSTR,
         hmod: HMODULE,
         fdwSound: DWORD,
+    ) -> BOOL;
+    pub fn sndPlaySoundA(
+        pszSound: LPCSTR,
+        fuSound: UINT,
+    ) -> BOOL;
+    pub fn sndPlaySoundW(
+        pszSound: LPCWSTR,
+        fuSound: UINT,
     ) -> BOOL;
 }
