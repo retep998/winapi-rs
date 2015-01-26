874ba9f2abe7150c9065c6bc8c070578c36e38b5
Peter Atashian <retep998@gmail.com>
2015-01-26 11:13:44 -0500

    Update list of crates.
    Signed-off-by: Peter Atashian <retep998@gmail.com>

--- src/lib.rs
+++ src/lib.rs
@@ -4,6 +4,7 @@
 #![no_std]
 #![experimental]
 #![allow(non_camel_case_types, non_snake_case, non_upper_case_globals, raw_pointer_derive)]
+#![warn(unused_qualifications)]
 
 //-------------------------------------------------------------------------------------------------
 // External crates
