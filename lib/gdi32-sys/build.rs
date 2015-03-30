5b4908dc6ae788f537c0b2ab27d46ba630cf30ee
Peter Atashian <retep998@gmail.com>
2015-03-29 20:50:12 -0400

    Bump
    Signed-off-by: Peter Atashian <retep998@gmail.com>

--- lib/gdi32-sys/build.rs
+++ lib/gdi32-sys/build.rs
@@ -1,5 +1,5 @@
 // Copyright © 2015, Peter Atashian
 // Licensed under the MIT License <LICENSE.md>
 fn main() {
-    println!("cargo:rustc-flags=-l gdi32");
+    println!("cargo:rustc-link-lib=dylib=gdi32");
 }
