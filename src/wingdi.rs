7b1e0c06dc402a0f0e6f08b20ebe7bafcf06b713
Jordan Miner <jminer7@gmail.com>
2015-05-09 00:08:59 -0500

    Add some bitmap functions and structs

--- src/wingdi.rs
+++ src/wingdi.rs
@@ -1,6 +1,129 @@
 // Copyright © 2015, Peter Atashian
 // Licensed under the MIT License <LICENSE.md>
 //! GDI procedure declarations, constant definitions and macros
+//572 (Win 7 SDK)
+#[repr(C)] #[derive(Clone, Copy, Debug)]
+pub struct BITMAP {
+    pub bmType: ::LONG,
+    pub bmWidth: ::LONG,
+    pub bmHeight: ::LONG,
+    pub bmWidthBytes: ::LONG,
+    pub bmPlanes: ::WORD,
+    pub bmBitsPixel: ::WORD,
+    pub bmBits: ::LPVOID,
+}
+pub type PBITMAP = *mut BITMAP;
+pub type NPBITMAP = *mut BITMAP;
+pub type LPBITMAP = *mut BITMAP;
+#[repr(C)] #[derive(Clone, Copy, Debug)]
+pub struct RGBQUAD {
+    pub rgbBlue: ::BYTE,
+    pub rgbGreen: ::BYTE,
+    pub rgbRed: ::BYTE,
+    pub rgbReserved: ::BYTE,
+}
+pub type LPRGBQUAD = *mut RGBQUAD;
+pub const CS_ENABLE: ::DWORD = 0x00000001;
+pub const CS_DISABLE: ::DWORD = 0x00000002;
+pub const CS_DELETE_TRANSFORM: ::DWORD = 0x00000003;
+pub const LCS_SIGNATURE: ::DWORD = 0x5053_4F43; // 'PSOC'
+pub const LCS_sRGB: LCSCSTYPE = 0x7352_4742; // 'sRGB'
+pub const LCS_WINDOWS_COLOR_SPACE: LCSCSTYPE = 0x5769_6E20; // 'Win '
+pub type LCSCSTYPE = ::LONG;
+pub const LCS_CALIBRATED_RGB: LCSCSTYPE = 0x00000000;
+pub type LCSGAMUTMATCH = ::LONG;
+pub const LCS_GM_BUSINESS: LCSGAMUTMATCH = 0x00000001;
+pub const LCS_GM_GRAPHICS: LCSGAMUTMATCH = 0x00000002;
+pub const LCS_GM_IMAGES: LCSGAMUTMATCH = 0x00000004;
+pub const LCS_GM_ABS_COLORIMETRIC: LCSGAMUTMATCH = 0x00000008;
+pub const CM_OUT_OF_GAMUT: ::BYTE = 255;
+pub const CM_IN_GAMUT: ::BYTE = 0;
+pub const ICM_ADDPROFILE: ::UINT = 1;
+pub const ICM_DELETEPROFILE: ::UINT = 2;
+pub const ICM_QUERYPROFILE: ::UINT = 3;
+pub const ICM_SETDEFAULTPROFILE: ::UINT = 4;
+pub const ICM_REGISTERICMATCHER: ::UINT = 5;
+pub const ICM_UNREGISTERICMATCHER: ::UINT = 6;
+pub const ICM_QUERYMATCH: ::UINT = 7;
+pub type FXPT16DOT16 = ::c_long;
+pub type LPFXPT16DOT16 = *mut ::c_long;
+pub type FXPT2DOT30 = ::c_long;
+pub type LPFXPT2DOT30 = *mut ::c_long;
+#[repr(C)] #[derive(Clone, Copy, Debug)]
+pub struct CIEXYZ {
+    pub ciexyzX: FXPT2DOT30,
+    pub ciexyzY: FXPT2DOT30,
+    pub ciexyzZ: FXPT2DOT30,
+}
+pub type LPCIEXYZ = *mut CIEXYZ;
+#[repr(C)] #[derive(Clone, Copy, Debug)]
+pub struct CIEXYZTRIPLE {
+    pub ciexyzRed: CIEXYZ,
+    pub ciexyzGreen: CIEXYZ,
+    pub ciexyzBlue: CIEXYZ,
+}
+pub type LPCIEXYZTRIPLE = *mut CIEXYZTRIPLE;
+//716 (Win 7 SDK)
+#[repr(C)] #[derive(Clone, Copy, Debug)]
+pub struct BITMAPINFOHEADER {
+    pub biSize: ::DWORD,
+    pub biWidth: ::LONG,
+    pub biHeight: ::LONG,
+    pub biPlanes: ::WORD,
+    pub biBitCount: ::WORD,
+    pub biCompression: ::DWORD,
+    pub biSizeImage: ::DWORD,
+    pub biXPelsPerMeter: ::LONG,
+    pub biYPelsPerMeter: ::LONG,
+    pub biClrUsed: ::DWORD,
+    pub biClrImportant: ::DWORD,
+}
+pub type LPBITMAPINFOHEADER = *mut BITMAPINFOHEADER;
+pub type PBITMAPINFOHEADER = *mut BITMAPINFOHEADER;
+#[repr(C)] #[derive(Clone, Copy, Debug)]
+pub struct BITMAPV5HEADER {
+    pub bV5Size: ::DWORD,
+    pub bV5Width: ::LONG,
+    pub bV5Height: ::LONG,
+    pub bV5Planes: ::WORD,
+    pub bV5BitCount: ::WORD,
+    pub bV5Compression: ::DWORD,
+    pub bV5SizeImage: ::DWORD,
+    pub bV5XPelsPerMeter: ::LONG,
+    pub bV5YPelsPerMeter: ::LONG,
+    pub bV5ClrUsed: ::DWORD,
+    pub bV5ClrImportant: ::DWORD,
+    pub bV5RedMask: ::DWORD,
+    pub bV5GreenMask: ::DWORD,
+    pub bV5BlueMask: ::DWORD,
+    pub bV5AlphaMask: ::DWORD,
+    pub bV5CSType: ::LONG, // LONG to match LOGCOLORSPACE
+    pub bV5Endpoints: CIEXYZTRIPLE,
+    pub bV5GammaRed: ::DWORD,
+    pub bV5GammaGreen: ::DWORD,
+    pub bV5GammaBlue: ::DWORD,
+    pub bV5Intent: ::LONG, // LONG to match LOGCOLORSPACE
+    pub bV5ProfileData: ::DWORD,
+    pub bV5ProfileSize: ::DWORD,
+    pub bV5Reserved: ::DWORD,
+}
+pub type LPBITMAPV5HEADER = *mut BITMAPV5HEADER;
+pub type PBITMAPV5HEADER = *mut BITMAPV5HEADER;
+pub const PROFILE_LINKED: ::LONG = 0x4C49_4E4B; // 'LINK'
+pub const PROFILE_EMBEDDED: ::LONG = 0x4D42_4544; // 'MBED'
+pub const BI_RGB: ::DWORD = 0;
+pub const BI_RLE8: ::DWORD = 1;
+pub const BI_RLE4: ::DWORD = 2;
+pub const BI_BITFIELDS: ::DWORD = 3;
+pub const BI_JPEG: ::DWORD = 4;
+pub const BI_PNG: ::DWORD = 5;
+#[repr(C)] // no Clone, Copy, or Debug because last field is special
+pub struct BITMAPINFO {
+    pub bmiHeader: BITMAPINFOHEADER,
+    pub bmiColors: [RGBQUAD; 1usize],
+}
+pub type LPBITMAPINFO = *mut BITMAPINFO;
+pub type PBITMAPINFO = *mut BITMAPINFO;
 //1438
 pub const LF_FACESIZE: usize = 32;
 //1595
@@ -8,6 +131,9 @@ pub const LF_FACESIZE: usize = 32;
 pub fn RGB (r: ::BYTE, g: ::BYTE, b: ::BYTE) -> ::COLORREF {
   r as ::COLORREF | ((g as ::COLORREF) << 8) | ((b as ::COLORREF) << 16)
 }
+//1906 (Win 7 SDK)
+pub const DIB_RGB_COLORS: ::UINT = 0;
+pub const DIB_PAL_COLORS: ::UINT = 1;
 
 // this type is weird because it's a hacky "unsized type"
 #[repr(C)]
