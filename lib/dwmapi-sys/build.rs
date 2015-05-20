468866b7b8b2551128786d0fef18df3736f5a960
Peter Atashian <retep998@gmail.com>
2015-05-20 14:49:23 -0400

    Factor out build script code
    Signed-off-by: Peter Atashian <retep998@gmail.com>

--- lib/dwmapi-sys/build.rs
+++ lib/dwmapi-sys/build.rs
@@ -1,14 +1,6 @@
 // Copyright © 2015, Peter Atashian
 // Licensed under the MIT License <LICENSE.md>
-use std::env::var;
+extern crate build;
 fn main() {
-    let target = var("TARGET").unwrap();
-    let target: Vec<_> = target.split('-').collect();
-    if target[2] == "windows" {
-        println!("cargo:rustc-link-lib=dylib=dwmapi");
-        if target[3] == "gnu" {
-            let dir = var("CARGO_MANIFEST_DIR").unwrap();
-            println!("cargo:rustc-link-search=native={}/{}", dir, target[0]);
-        }
-    }
+    build::link("dwmapi", true)
 }
