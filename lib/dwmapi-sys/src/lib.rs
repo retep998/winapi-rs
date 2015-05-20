e83a9f4ff2a82662507e7e495629a92ab361c2a1
Peter Atashian <retep998@gmail.com>
2015-05-20 15:46:12 -0400

    Various cleanup Remove Default impls because they aren't on anything else plus they just make compile times longer. Use `std::mem::zeroed` if you have to. Added missing copyright/license header. Also added list of exports to dwmapi
    Signed-off-by: Peter Atashian <retep998@gmail.com>

--- lib/dwmapi-sys/src/lib.rs
+++ lib/dwmapi-sys/src/lib.rs
@@ -5,5 +5,34 @@
 extern crate winapi;
 use winapi::*;
 extern "system" {
-    pub fn DwmEnableBlurBehindWindow(hWnd: HWND, pBlurBehind: *const winapi::dwmapi::DWM_BLURBEHIND) -> HRESULT;
+    // pub fn DwmAttachMilContent();
+    // pub fn DwmDefWindowProc();
+    // pub fn DwmDetachMilContent();
+    pub fn DwmEnableBlurBehindWindow(hWnd: HWND, pBlurBehind: *const DWM_BLURBEHIND) -> HRESULT;
+    // pub fn DwmEnableComposition();
+    // pub fn DwmEnableMMCSS();
+    // pub fn DwmExtendFrameIntoClientArea();
+    // pub fn DwmFlush();
+    // pub fn DwmGetColorizationColor();
+    // pub fn DwmGetCompositionTimingInfo();
+    // pub fn DwmGetGraphicsStreamClient();
+    // pub fn DwmGetGraphicsStreamTransformHint();
+    // pub fn DwmGetTransportAttributes();
+    // pub fn DwmGetWindowAttribute();
+    // pub fn DwmInvalidateIconicBitmaps();
+    // pub fn DwmIsCompositionEnabled();
+    // pub fn DwmModifyPreviousDxFrameDuration();
+    // pub fn DwmQueryThumbnailSourceSize();
+    // pub fn DwmRegisterThumbnail();
+    // pub fn DwmRenderGesture();
+    // pub fn DwmSetDxFrameDuration();
+    // pub fn DwmSetIconicLivePreviewBitmap();
+    // pub fn DwmSetIconicThumbnail();
+    // pub fn DwmSetPresentParameters();
+    // pub fn DwmSetWindowAttribute();
+    // pub fn DwmShowContact();
+    // pub fn DwmTetherContact();
+    // pub fn DwmTransitionOwnedWindow();
+    // pub fn DwmUnregisterThumbnail();
+    // pub fn DwmUpdateThumbnailProperties();
 }
--- src/processthreadsapi.rs
+++ src/processthreadsapi.rs
@@ -1,3 +1,5 @@
+// Copyright © 2015, Peter Atashian
+// Licensed under the MIT License <LICENSE.md>
 #[repr(C)] #[derive(Clone, Copy, Debug)]
 pub struct STARTUPINFOA {
     pub cb: ::DWORD,
@@ -20,18 +22,6 @@ pub struct STARTUPINFOA {
     pub hStdError: ::HANDLE,
 }
 pub type LPSTARTUPINFOA = *mut STARTUPINFOA;
-impl ::std::default::Default for STARTUPINFOA {
-    fn default() -> STARTUPINFOA {
-        STARTUPINFOA {
-            cb: 0, lpReserved: ::std::ptr::null_mut(), lpDesktop: ::std::ptr::null_mut(),
-            lpTitle: ::std::ptr::null_mut(), dwX: 0, dwY: 0, dwXSize: 0, dwYSize: 0,
-            dwXCountChars: 0, dwYCountChars: 0, dwFillAttribute: 0, dwFlags: 0, wShowWindow: 0,
-            cbReserved2: 0, lpReserved2: ::std::ptr::null_mut(),
-            hStdInput: ::std::ptr::null_mut(), hStdOutput: ::std::ptr::null_mut(),
-            hStdError: ::std::ptr::null_mut(),
-        }
-    }
-}
 #[repr(C)] #[derive(Clone, Copy, Debug)]
 pub struct STARTUPINFOW {
     pub cb: ::DWORD,
@@ -52,17 +42,5 @@ pub struct STARTUPINFOW {
     pub hStdInput: ::HANDLE,
     pub hStdOutput: ::HANDLE,
     pub hStdError: ::HANDLE,
-}
-impl ::std::default::Default for STARTUPINFOW {
-    fn default() -> STARTUPINFOW {
-        STARTUPINFOW {
-            cb: 0, lpReserved: ::std::ptr::null_mut(), lpDesktop: ::std::ptr::null_mut(),
-            lpTitle: ::std::ptr::null_mut(), dwX: 0, dwY: 0, dwXSize: 0, dwYSize: 0,
-            dwXCountChars: 0, dwYCountChars: 0, dwFillAttribute: 0, dwFlags: 0, wShowWindow: 0,
-            cbReserved2: 0, lpReserved2: ::std::ptr::null_mut(),
-            hStdInput: ::std::ptr::null_mut(), hStdOutput: ::std::ptr::null_mut(),
-            hStdError: ::std::ptr::null_mut(),
-        }
-    }
 }
 pub type LPSTARTUPINFOW = *mut STARTUPINFOW;
