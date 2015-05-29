2aade5704b572d3f651f2a42a0d91eddd21d8c37
Peter Atashian <retep998@gmail.com>
2015-05-29 14:17:33 -0400

    Add psapi
    Signed-off-by: Peter Atashian <retep998@gmail.com>

--- lib/psapi-sys/src/lib.rs
+++ lib/psapi-sys/src/lib.rs
@@ -1,9 +1,39 @@
 // Copyright © 2015, Peter Atashian
 // Licensed under the MIT License <LICENSE.md>
 //! FFI bindings to psapi.
-#![no_std]
-#![experimental]
+#![cfg(windows)]
 extern crate winapi;
 use winapi::*;
 extern "system" {
+    // pub fn EmptyWorkingSet();
+    // pub fn EnumDeviceDrivers();
+    // pub fn EnumPageFilesA();
+    // pub fn EnumPageFilesW();
+    // pub fn EnumProcessModules();
+    // pub fn EnumProcessModulesEx();
+    // pub fn EnumProcesses();
+    // pub fn GetDeviceDriverBaseNameA();
+    // pub fn GetDeviceDriverBaseNameW();
+    // pub fn GetDeviceDriverFileNameA();
+    // pub fn GetDeviceDriverFileNameW();
+    // pub fn GetMappedFileNameA();
+    // pub fn GetMappedFileNameW();
+    // pub fn GetModuleBaseNameA();
+    // pub fn GetModuleBaseNameW();
+    // pub fn GetModuleFileNameExA();
+    // pub fn GetModuleFileNameExW();
+    // pub fn GetModuleInformation();
+    // pub fn GetPerformanceInfo();
+    pub fn GetProcessImageFileNameA(
+        hProcess: HANDLE, lpImageFileName: LPSTR, nSize: DWORD,
+    ) -> DWORD;
+    pub fn GetProcessImageFileNameW(
+        hProcess: HANDLE, lpImageFileName: LPWSTR, nSize: DWORD,
+    ) -> DWORD;
+    // pub fn GetProcessMemoryInfo();
+    // pub fn GetWsChanges();
+    // pub fn GetWsChangesEx();
+    // pub fn InitializeProcessForWsWatch();
+    // pub fn QueryWorkingSet();
+    // pub fn QueryWorkingSetEx();
 }
