dbb948ea2747aeee9db2ddad7da1f4f10a0a08a4
Daggerbot <daggerbot@gmail.com>
2015-06-25 08:25:19 -0700

    Added UNICODE_NOCHAR.

--- src/winuser.rs
+++ src/winuser.rs
@@ -1,6 +1,8 @@
 // Copyright © 2015, Peter Atashian
 // Licensed under the MIT License <LICENSE.md>
 //! USER procedure declarations, constant definitions and macros
+//1913
+pub const UNICODE_NOCHAR: ::WPARAM = 0xffff;
 pub type HDWP = *mut ::HANDLE;
 //2193
 pub const WHEEL_DELTA: ::DWORD = 120;
