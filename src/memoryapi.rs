7364d82780173a81de5dfd56be7a5e351557f7b3
Mike Boutin <mike.boutin@gmail.com>
2015-07-05 11:58:30 -0400

    Add MapViewOfFile() constants.

---
+++ src/memoryapi.rs
@@ -0,0 +1,9 @@
+// Copyright © 2015, Peter Atashian
+// Licensed under the MIT License <LICENSE.md>
+//! ApiSet Contract for api-ms-win-core-memory-l1-1-0
+pub const FILE_MAP_WRITE: ::DWORD = ::SECTION_MAP_WRITE;
+pub const FILE_MAP_READ: ::DWORD = ::SECTION_MAP_READ;
+pub const FILE_MAP_ALL_ACCESS: ::DWORD = ::SECTION_ALL_ACCESS;
+pub const FILE_MAP_EXECUTE: ::DWORD = ::SECTION_MAP_EXECUTE_EXPLICIT;
+pub const FILE_MAP_COPY: ::DWORD = 0x00000001;
+pub const FILE_MAP_RESERVE: ::DWORD = 0x80000000;
