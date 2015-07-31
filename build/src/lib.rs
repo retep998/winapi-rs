5f37c3647dcfc0527dfbf0434b7920684d992429
Cody P Schafer <dev@codyps.com>
2015-07-31 15:00:55 -0400

    build: avoid panicing build script if target does split to >2 elements

--- build/src/lib.rs
+++ build/src/lib.rs
@@ -4,7 +4,7 @@ pub fn link(name: &str, bundled: bool) {
     use std::env::var;
     let target = var("TARGET").unwrap();
     let target: Vec<_> = target.split('-').collect();
-    if target[2] == "windows" {
+    if target.len() > 2 && target[2] == "windows" {
         println!("cargo:rustc-link-lib=dylib={}", name);
         if bundled && target[3] == "gnu" {
             let dir = var("CARGO_MANIFEST_DIR").unwrap();
