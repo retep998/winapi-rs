774332ba6af3971df3c4fe04b99fe8edfb8d468b
red75prime <red75prim@gmail.com>
2015-09-07 15:46:07 +0500

    Style. One enum goes to dxgitype.rs

--- src/dxgi.rs
+++ src/dxgi.rs
@@ -263,8 +263,10 @@ interface IDXGISwapChain1(IDXGISwapChain1Vtbl): IDXGISwapChain(IDXGISwapChainVtb
     fn GetFullscreenDesc(&mut self, pDesc: *mut ::DXGI_SWAP_CHAIN_FULLSCREEN_DESC) -> ::HRESULT,
     fn GetHwnd(&mut self, pHwnd: *mut ::HWND) -> ::HRESULT,
     fn GetCoreWindow(&mut self, refiid: ::REFIID, ppUnk: *mut *mut ::c_void) -> ::HRESULT,
-    fn Present1(&mut self, SyncInterval: ::UINT, PresentFlags: ::UINT,
-        pPresentParameters: *const DXGI_PRESENT_PARAMETERS) -> ::HRESULT,
+    fn Present1(
+        &mut self, SyncInterval: ::UINT, PresentFlags: ::UINT,
+        pPresentParameters: *const DXGI_PRESENT_PARAMETERS
+    ) -> ::HRESULT,
     fn IsTemporaryMonoSupported(&mut self) -> ::BOOL,
     fn GetRestrictToOutput(&mut self, ppRestrictToOutput: *mut *mut ::IDXGIOutput) -> ::HRESULT,
     fn SetBackgroundColor(&mut self, pColor: *const ::DXGI_RGBA) -> ::HRESULT,
@@ -293,24 +295,6 @@ interface IDXGISwapChain2(IDXGISwapChain2Vtbl): IDXGISwapChain1(IDXGISwapChain1V
     fn SetMatrixTransform(&mut self, pMatrix: *const DXGI_MATRIX_3X2_F) -> ::HRESULT,
     fn GetMatrixTransform(&mut self, pMatrix: *mut DXGI_MATRIX_3X2_F) -> ::HRESULT
 });
-
-#[repr(i32)] #[derive(Copy, Clone, Debug)] #[allow(unused_qualifications)]
-pub enum DXGI_COLOR_SPACE_TYPE {
-    DXGI_COLOR_SPACE_RGB_FULL_G22_NONE_P709,
-    DXGI_COLOR_SPACE_RGB_FULL_G10_NONE_P709,
-    DXGI_COLOR_SPACE_RGB_STUDIO_G22_NONE_P709,
-    DXGI_COLOR_SPACE_RGB_STUDIO_G22_NONE_P2020,
-    DXGI_COLOR_SPACE_RESERVED,
-    DXGI_COLOR_SPACE_YCBCR_FULL_G22_NONE_P709_X601,
-    DXGI_COLOR_SPACE_YCBCR_STUDIO_G22_LEFT_P601,
-    DXGI_COLOR_SPACE_YCBCR_FULL_G22_LEFT_P601,
-    DXGI_COLOR_SPACE_YCBCR_STUDIO_G22_LEFT_P709,
-    DXGI_COLOR_SPACE_YCBCR_FULL_G22_LEFT_P709,
-    DXGI_COLOR_SPACE_YCBCR_STUDIO_G22_LEFT_P2020,
-    DXGI_COLOR_SPACE_YCBCR_FULL_G22_LEFT_P2020,
-}
-
-pub use self::DXGI_COLOR_SPACE_TYPE::*;
 
 RIDL!(
 interface IDXGISwapChain3(IDXGISwapChain3Vtbl): IDXGISwapChain2(IDXGISwapChain2Vtbl) {
