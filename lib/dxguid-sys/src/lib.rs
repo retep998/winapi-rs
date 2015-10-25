76147286f4c5bdd375ef2c37620391c633c93633
red75prime <red75prim@gmail.com>
2015-10-25 22:34:44 +0500

    remove no_std

--- lib/dxguid-sys/src/lib.rs
+++ lib/dxguid-sys/src/lib.rs
@@ -1,8 +1,7 @@
 // Copyright © 2015, Peter Atashian
 // Licensed under the MIT License <LICENSE.md>
 //! FFI bindings to dxguid.
-#![no_std]
-#![experimental]
+
 extern crate winapi;
 use winapi::*;
 extern "system" {
