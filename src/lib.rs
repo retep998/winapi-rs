fefd06dd1d6dcc9b399fdbbe0b3d3b1a2fa7edad
Peter Atashian <retep998@gmail.com>
2016-05-10 11:12:17 -0400

    Finish cleaning out lib.rs
    Signed-off-by: Peter Atashian <retep998@gmail.com>

--- src/lib.rs
+++ src/lib.rs
@@ -30,6 +30,7 @@ pub use basetsd::*;
 pub use bcrypt::*;
 pub use cfg::*;
 pub use cfgmgr32::*;
+pub use combaseapi::*;
 pub use commctrl::*;
 pub use commdlg::*;
 pub use corsym::*;
@@ -103,8 +104,10 @@ pub use objidl::*;
 pub use objidlbase::*;
 pub use olectl::*;
 pub use pdh::*;
+pub use playsoundapi::*;
 pub use processsnapshot::*;
 pub use processthreadsapi::*;
+pub use propidl::*;
 pub use propsys::*;
 pub use prsht::*;
 pub use psapi::*;
@@ -125,6 +128,7 @@ pub use shtypes::*;
 pub use spapidef::*;
 pub use sqltypes::*;
 pub use sspi::*;
+pub use strmif::*;
 pub use subauth::*;
 pub use synchapi::*;
 pub use sysinfoapi::*;
@@ -156,6 +160,7 @@ pub use winioctl::*;
 pub use winnetwk::*;
 pub use winnls::*;
 pub use winnt::*;
+pub use winreg::*;
 pub use winscard::*;
 pub use winsmcrd::*;
 pub use winsock2::*;
@@ -179,6 +184,7 @@ pub mod basetsd;
 pub mod bcrypt;
 pub mod cfg;
 pub mod cfgmgr32;
+pub mod combaseapi;
 pub mod commctrl;
 pub mod commdlg;
 pub mod corsym;
@@ -252,8 +258,10 @@ pub mod objidl;
 pub mod objidlbase;
 pub mod olectl;
 pub mod pdh;
+pub mod playsoundapi;
 pub mod processsnapshot;
 pub mod processthreadsapi;
+pub mod propidl;
 pub mod propsys;
 pub mod prsht;
 pub mod psapi;
@@ -274,6 +282,7 @@ pub mod shtypes;
 pub mod spapidef;
 pub mod sqltypes;
 pub mod sspi;
+pub mod strmif;
 pub mod subauth;
 pub mod synchapi;
 pub mod sysinfoapi;
@@ -305,6 +314,7 @@ pub mod winioctl;
 pub mod winnetwk;
 pub mod winnls;
 pub mod winnt;
+pub mod winreg;
 pub mod winscard;
 pub mod winsmcrd;
 pub mod winsock2;
@@ -334,97 +344,3 @@ pub type wchar_t = c_ushort;
 pub type size_t = c_uint;
 #[cfg(target_arch = "x86_64")]
 pub type size_t = __uint64;
-//-------------------------------------------------------------------------------------------------
-// strmif.h
-//-------------------------------------------------------------------------------------------------
-pub type REFERENCE_TIME = LONGLONG;
-//-------------------------------------------------------------------------------------------------
-// propidl.h
-//-------------------------------------------------------------------------------------------------
-STRUCT!{struct PROPVARIANT {
-    vt: VARTYPE,
-    wReserved1: WORD,
-    wReserved2: WORD,
-    wReserved3: WORD,
-    data: [u8; 16],
-}}
-//-------------------------------------------------------------------------------------------------
-// combaseapi.h
-// Base Component Object Model defintions.
-//-------------------------------------------------------------------------------------------------
-pub const CLSCTX_INPROC_SERVER: DWORD = 0x1;
-pub const CLSCTX_INPROC_HANDLER: DWORD = 0x2;
-pub const CLSCTX_LOCAL_SERVER: DWORD = 0x4;
-pub const CLSCTX_REMOTE_SERVER: DWORD = 0x10;
-pub const CLSCTX_SERVER: DWORD = CLSCTX_INPROC_SERVER | CLSCTX_LOCAL_SERVER |
-                                 CLSCTX_REMOTE_SERVER;
-pub const CLSCTX_ALL: DWORD = CLSCTX_INPROC_HANDLER | CLSCTX_SERVER;
-STRUCT!{struct ServerInformation {
-    dwServerPid: DWORD,
-    dwServerTid: DWORD,
-    ui64ServerAddress: UINT64,
-}}
-pub type PServerInformation = *mut ServerInformation;
-DECLARE_HANDLE!(CO_MTA_USAGE_COOKIE, CO_MTA_USAGE_COOKIE__);
-//-------------------------------------------------------------------------------------------------
-// playsoundapi.h
-// ApiSet Contract for api-ms-win-mm-playsound-l1-1-0
-//-------------------------------------------------------------------------------------------------
-pub const SND_SYNC: DWORD = 0x0000;
-pub const SND_ASYNC: DWORD = 0x0001;
-pub const SND_NODEFAULT: DWORD = 0x0002;
-pub const SND_MEMORY: DWORD = 0x0004;
-pub const SND_LOOP: DWORD = 0x0008;
-pub const SND_NOSTOP: DWORD = 0x0010;
-pub const SND_NOWAIT: DWORD = 0x00002000;
-pub const SND_ALIAS: DWORD = 0x00010000;
-pub const SND_ALIAS_ID: DWORD = 0x00110000;
-pub const SND_FILENAME: DWORD = 0x00020000;
-pub const SND_RESOURCE: DWORD = 0x00040004;
-pub const SND_PURGE: DWORD = 0x0040;
-pub const SND_APPLICATION: DWORD = 0x0080;
-pub const SND_SENTRY: DWORD = 0x00080000;
-pub const SND_RING: DWORD = 0x00100000;
-pub const SND_SYSTEM: DWORD = 0x00200000;
-//-------------------------------------------------------------------------------------------------
-// winreg.h
-// Registry API procedure declarations, constant definitions and macros
-//-------------------------------------------------------------------------------------------------
-pub type REGSAM = ACCESS_MASK;
-STRUCT!{struct VALENTA {
-    ve_valuename: LPSTR,
-    ve_valuelen: DWORD,
-    ve_valueptr: DWORD_PTR,
-    ve_type: DWORD,
-}}
-pub type PVALENTA = *mut VALENTA;
-STRUCT!{struct VALENTW {
-    ve_valuename: LPWSTR,
-    ve_valuelen: DWORD,
-    ve_valueptr: DWORD_PTR,
-    ve_type: DWORD,
-}}
-pub type PVALENTW = *mut VALENTW;
-pub const HKEY_CLASSES_ROOT: HKEY = 0x80000000 as HKEY;
-pub const HKEY_CURRENT_USER: HKEY = 0x80000001 as HKEY;
-pub const HKEY_LOCAL_MACHINE: HKEY = 0x80000002 as HKEY;
-pub const HKEY_USERS: HKEY = 0x80000003 as HKEY;
-pub const HKEY_PERFORMANCE_DATA: HKEY = 0x80000004 as HKEY;
-pub const HKEY_PERFORMANCE_TEXT: HKEY = 0x80000050 as HKEY;
-pub const HKEY_PERFORMANCE_NLSTEXT: HKEY = 0x80000060 as HKEY;
-pub const HKEY_CURRENT_CONFIG: HKEY = 0x80000005 as HKEY;
-pub const HKEY_DYN_DATA: HKEY = 0x80000006 as HKEY;
-pub const HKEY_CURRENT_USER_LOCAL_SETTINGS: HKEY = 0x80000007 as HKEY;
-pub const REG_MUI_STRING_TRUNCATE: DWORD = 0x00000001;
-pub const RRF_RT_REG_NONE: DWORD = 0x00000001;
-pub const RRF_RT_REG_SZ: DWORD = 0x00000002;
-pub const RRF_RT_REG_EXPAND_SZ: DWORD = 0x00000004;
-pub const RRF_RT_REG_BINARY: DWORD = 0x00000008;
-pub const RRF_RT_REG_DWORD: DWORD = 0x00000010;
-pub const RRF_RT_REG_MULTI_SZ: DWORD = 0x00000020;
-pub const RRF_RT_REG_QWORD: DWORD = 0x00000040;
-pub const RRF_RT_DWORD: DWORD = RRF_RT_REG_BINARY|RRF_RT_REG_DWORD;
-pub const RRF_RT_QWORD: DWORD = RRF_RT_REG_BINARY|RRF_RT_REG_QWORD;
-pub const RRF_RT_ANY: DWORD = 0x0000ffff;
-pub const RRF_NOEXPAND: DWORD = 0x10000000;
-pub const RRF_ZEROONFAILURE: DWORD = 0x20000000;
