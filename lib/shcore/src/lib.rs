1a4158e31b67d2bc8dec0348e4d1e18e51fec14d
Daniel Abramov <inetcrack2@gmail.com>
2016-09-02 11:54:11 +0200

    Fix compile and link errors in shcore lib

--- lib/shcore/src/lib.rs
+++ lib/shcore/src/lib.rs
@@ -5,9 +5,9 @@
 extern crate winapi;
 use winapi::*;
 extern "system" {
-    pub fn GetDpiForMonitor(hmonitor: HMONITOR, dpiType: MONITOR_DPI_TYPE, dpiX: *UINT, dpiY: *UINT) -> HRESULT
+    pub fn GetDpiForMonitor(hmonitor: HMONITOR, dpiType: MONITOR_DPI_TYPE, dpiX: *mut UINT, dpiY: *mut UINT) -> HRESULT;
     pub fn GetDpiForShellUiComponent(component: SHELL_UI_COMPONENT) -> UINT;
-    pub fn GetProcessDPIAwareness(hProcess: HANDLE, value: *PROCESS_DPI_AWARENESS) -> HRESULT;
+    pub fn GetProcessDPIAwareness(hProcess: HANDLE, value: *mut PROCESS_DPI_AWARENESS) -> HRESULT;
     pub fn SetProcessDPIAwareness(value: PROCESS_DPI_AWARENESS) -> HRESULT;
     pub fn LogicalToPhysicalPointForPerMonitorDPI(hwnd: HWND, lpPoint: LPPOINT) -> BOOL;
     pub fn PhysicalToLogicalPointForPerMonitorDPI(hwnd: HWND, lpPoint: LPPOINT) -> BOOL;
