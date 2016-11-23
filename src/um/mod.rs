564de024b2b5d2c702b7d5d6e9b2fb57da6806f5
Victor Koenders <victor.koenders@gmail.com>
2016-11-23 20:08:47 +0100

    Moved gl.rs to the correct folder and made the um-gl feature

--- src/um/mod.rs
+++ src/um/mod.rs
@@ -4,7 +4,11 @@
 // <LICENSE-MIT or http://opensource.org/licenses/MIT>, at your option.
 // All files in the project carrying such notice may not be copied, modified, or distributed
 // except according to those terms.
+
+pub mod gl;
+
 //! Headers for user mode only
+
 #[cfg(feature = "um-audioclient")] pub mod audioclient;
 #[cfg(feature = "um-audiosessiontypes")] pub mod audiosessiontypes;
 #[cfg(feature = "um-cfgmgr32")] pub mod cfgmgr32;
