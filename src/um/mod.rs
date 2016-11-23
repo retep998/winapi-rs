6835967a078a406610488504fb8f7b24ba3685c4
Victor Koenders <victor.koenders@gmail.com>
2016-11-23 20:19:31 +0100

    Moved doc header comment in the right position.
    That's what you get for changing things without testing them

--- src/um/mod.rs
+++ src/um/mod.rs
@@ -4,10 +4,9 @@
 // <LICENSE-MIT or http://opensource.org/licenses/MIT>, at your option.
 // All files in the project carrying such notice may not be copied, modified, or distributed
 // except according to those terms.
+//! Headers for user mode only
 
 pub mod gl;
-
-//! Headers for user mode only
 
 #[cfg(feature = "um-audioclient")] pub mod audioclient;
 #[cfg(feature = "um-audiosessiontypes")] pub mod audiosessiontypes;
