2885e1c8069e6ac013bbc37c8b2c0ac6e2e381e3
Guillaume Gomez <guillaume1.gomez@gmail.com>
2017-03-27 20:08:49 +0200

    Port winusb to new winapi version

--- src/um/winusb.rs
+++ src/um/winusb.rs
@@ -4,29 +4,35 @@
 // <LICENSE-MIT or http://opensource.org/licenses/MIT>, at your option.
 // All files in the project carrying such notice may not be copied, modified, or distributed
 // except according to those terms.
+
 //! FFI bindings to winusb.
-pub type WINUSB_INTERFACE_HANDLE = ::PVOID;
-pub type PWINUSB_INTERFACE_HANDLE = *mut ::PVOID;
-pub type WINUSB_ISOCH_BUFFER_HANDLE = ::PVOID;
-pub type PWINUSB_ISOCH_BUFFER_HANDLE = *mut ::PVOID;
+
+use shared::minwindef::{UCHAR, USHORT};
+use um::winnt::PVOID;
+
+pub type WINUSB_INTERFACE_HANDLE = PVOID;
+pub type PWINUSB_INTERFACE_HANDLE = *mut PVOID;
+pub type WINUSB_ISOCH_BUFFER_HANDLE = PVOID;
+pub type PWINUSB_ISOCH_BUFFER_HANDLE = *mut PVOID;
+
 STRUCT!{#[repr(packed)] struct WINUSB_SETUP_PACKET {
-    RequestType: ::UCHAR,
-    Request: ::UCHAR,
-    Value: ::USHORT,
-    Index: ::USHORT,
-    Length: ::USHORT,
+    RequestType: UCHAR,
+    Request: UCHAR,
+    Value: USHORT,
+    Index: USHORT,
+    Length: USHORT,
 }}
 pub type PWINUSB_SETUP_PACKET = *mut WINUSB_SETUP_PACKET;
 
 STRUCT!{struct USB_INTERFACE_DESCRIPTOR {
-    bLength: ::UCHAR,
-    bDescriptorType: ::UCHAR,
-    bInterfaceNumber: ::UCHAR,
-    bAlternateSetting: ::UCHAR,
-    bNumEndpoints: ::UCHAR,
-    bInterfaceClass: ::UCHAR,
-    bInterfaceSubClass: ::UCHAR,
-    bInterfaceProtocol: ::UCHAR,
-    iInterface: ::UCHAR,
+    bLength: UCHAR,
+    bDescriptorType: UCHAR,
+    bInterfaceNumber: UCHAR,
+    bAlternateSetting: UCHAR,
+    bNumEndpoints: UCHAR,
+    bInterfaceClass: UCHAR,
+    bInterfaceSubClass: UCHAR,
+    bInterfaceProtocol: UCHAR,
+    iInterface: UCHAR,
 }}
 pub type PUSB_INTERFACE_DESCRIPTOR = *mut USB_INTERFACE_DESCRIPTOR;
