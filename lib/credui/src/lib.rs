4e5f8377c5994fe4a4fb823089710cc7f5945cdf
Bryant Mairs <bryant@mai.rs>
2017-05-06 16:40:04 -0700

    Remove finished libs

--- lib/credui/src/lib.rs
+++
@@ -1,8 +0,0 @@
-// Copyright © 2015, Peter Atashian
-// Licensed under the MIT License <LICENSE.md>
-//! FFI bindings to credui.
-#![cfg(windows)]
-extern crate winapi;
-use winapi::*;
-extern "system" {
-}
