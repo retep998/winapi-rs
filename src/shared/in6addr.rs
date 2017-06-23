1c195c61e88d08a6c189739da3c0ca3f365b43c3
Susurrus <bryant@mai.rs>
2017-06-23 12:48:26 -0700

    Finishing off the rest of src/*.rs (#455)
    * Migrate um/winhttp to 0.3
    
    * Migrate shared/rpc over to 0.3
    
    * Migrate rpcdce over to 0.3
    
    * Migrate um/vss to 0.3
    
    * Migrate um/vswriter to 0.3
    
    * Migrate um/vsbackup to 0.3
    
    * Migrate winnetwk to 0.3
    
    * Migrate shared/wnnc to 0.3
    
    * Migrate um/tlhelp32 to 0.3
    
    * Migrate um/ws2spi to 0.3
    
    * Migrate shared/ws2ipdef to 0.3
    
    * Migrate um/ws2tcpip and shared/in6addr to 0.3
    
    * Migrate um/sysinfoapi to 0.3
    
    * Migrate um/playsoundapi to 0.3
    
    * Migrate um/restrictederrorinfo to 0.3
    
    * Migrate um/shellscalingapi to 0.3
    
    * Migrate winrt/roerrorapi to 0.3
    
    * Migrate um/threadpoolapiset to 0.3
    
    * Migrate timezoneapi to 0.3

--- src/shared/in6addr.rs
+++ src/shared/in6addr.rs
@@ -4,6 +4,17 @@
 // <LICENSE-MIT or http://opensource.org/licenses/MIT>, at your option.
 // All files in the project carrying such notice may not be copied, modified, or distributed
 // except according to those terms.
-// Master include file for RPC applications.
-pub type I_RPC_HANDLE = *mut ::c_void;
-pub type RPC_STATUS = ::c_long;
+
+use shared::minwindef::{UCHAR, USHORT};
+
+UNION2!{union in6_addr_u {
+    [u16; 8],
+    Byte Byte_mut: [UCHAR; 16],
+    Word Word_mut: [USHORT; 8],
+}}
+STRUCT!{struct in6_addr {
+    u: in6_addr_u,
+}}
+pub type IN6_ADDR = in6_addr;
+pub type PIN6_ADDR = *mut IN6_ADDR;
+pub type LPIN6_ADDR = *mut IN6_ADDR;
