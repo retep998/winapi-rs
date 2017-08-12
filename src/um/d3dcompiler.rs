f826799d3e4fd38d4f63efa336dfd7bcb55363ad
Susurrus <bryant@mai.rs>
2017-08-11 17:39:12 -0700

    Migrate another chunk of DLL functions to 0.3 (#484)
    * Migrate d3dcompiler to 0.3
    
    * Migrate shell32 functions to 0.3
    
    * Migrate http functions to 0.3
    
    * Migrate runtimeobject to 0.3
    
    * Migrate winhttp to 0.3
    
    * Migrate winmm functions to 0.3

--- src/um/d3dcompiler.rs
+++ src/um/d3dcompiler.rs
@@ -4,11 +4,26 @@
 // <LICENSE-MIT or http://opensource.org/licenses/MIT>, at your option.
 // All files in the project carrying such notice may not be copied, modified, or distributed
 // except according to those terms.
-use shared::basetsd::{SIZE_T};
-use shared::minwindef::{DWORD, LPCVOID};
-use um::d3dcommon::{ID3DInclude};
+use ctypes::c_void;
+use shared::basetsd::SIZE_T;
+use shared::guiddef::REFIID;
+use shared::minwindef::{BOOL, DWORD, LPCVOID, LPVOID, UINT};
+use um::d3d11shader::{ID3D11FunctionLinkingGraph, ID3D11Linker, ID3D11Module};
+use um::d3dcommon::{D3D_SHADER_MACRO, ID3DBlob, ID3DInclude};
+use um::winnt::{HRESULT, LPCSTR, LPCWSTR};
 pub const D3DCOMPILER_DLL: &'static str = "d3dcompiler_47.dll";
 pub const D3D_COMPILER_VERSION: DWORD = 47;
+extern "system" {
+    pub fn D3DReadFileToBlob(
+        pFileName: LPCWSTR,
+        ppContents: *mut *mut ID3DBlob,
+    ) -> HRESULT;
+    pub fn D3DWriteBlobToFile(
+        pBlob: *mut ID3DBlob,
+        pFileName: LPCWSTR,
+        bOverwrite: BOOL,
+    ) -> HRESULT;
+}
 pub const D3DCOMPILE_DEBUG: DWORD = 1 << 0;
 pub const D3DCOMPILE_SKIP_VALIDATION: DWORD = 1 << 1;
 pub const D3DCOMPILE_SKIP_OPTIMIZATION: DWORD = 1 << 2;
@@ -36,9 +51,79 @@ pub const D3DCOMPILE_ALL_RESOURCES_BOUND: DWORD = 1 << 21;
 pub const D3DCOMPILE_EFFECT_CHILD_EFFECT: DWORD = 1 << 0;
 pub const D3DCOMPILE_EFFECT_ALLOW_SLOW_OPS: DWORD = 1 << 1;
 pub const D3D_COMPILE_STANDARD_FILE_INCLUDE: *mut ID3DInclude = 1 as *mut ID3DInclude;
+extern "system" {
+    pub fn D3DCompile(
+        pSrcData: LPCVOID,
+        SrcDataSize: SIZE_T,
+        pSourceName: LPCSTR,
+        pDefines: *const D3D_SHADER_MACRO,
+        pInclude: *mut ID3DInclude,
+        pEntrypoint: LPCSTR,
+        pTarget: LPCSTR,
+        Flags1: UINT,
+        Flags2: UINT,
+        ppCode: *mut *mut ID3DBlob,
+        ppErrorMsgs: *mut *mut ID3DBlob,
+    ) -> HRESULT;
+}
 pub const D3DCOMPILE_SECDATA_MERGE_UAV_SLOTS: DWORD = 0x00000001;
 pub const D3DCOMPILE_SECDATA_PRESERVE_TEMPLATE_SLOTS: DWORD = 0x00000002;
 pub const D3DCOMPILE_SECDATA_REQUIRE_TEMPLATE_MATCH: DWORD = 0x00000004;
+extern "system" {
+    pub fn D3DCompile2(
+        pSrcData: LPCVOID,
+        SrcDataSize: SIZE_T,
+        pSourceName: LPCSTR,
+        pDefines: *const D3D_SHADER_MACRO,
+        pInclude: *mut ID3DInclude,
+        pEntrypoint: LPCSTR,
+        pTarget: LPCSTR,
+        Flags1: UINT,
+        Flags2: UINT,
+        SecondaryDataFlags: UINT,
+        pSecondaryData: LPCVOID,
+        SecondaryDataSize: SIZE_T,
+        ppCode: *mut *mut ID3DBlob,
+        ppErrorMsgs: *mut *mut ID3DBlob,
+    ) -> HRESULT;
+    pub fn D3DCompileFromFile(
+        pFileName: LPCWSTR,
+        pDefines: *const D3D_SHADER_MACRO,
+        pInclude: *mut ID3DInclude,
+        pEntrypoint: LPCSTR,
+        pTarget: LPCSTR,
+        Flags1: UINT,
+        Flags2: UINT,
+        ppCode: *mut *mut ID3DBlob,
+        ppErrorMsgs: *mut *mut ID3DBlob,
+    ) -> HRESULT;
+    pub fn D3DPreprocess(
+        pSrcData: LPCVOID,
+        SrcDataSize: SIZE_T,
+        pSourceName: LPCSTR,
+        pDefines: *const D3D_SHADER_MACRO,
+        pInclude: *mut ID3DInclude,
+        ppCodeText: *mut *mut ID3DBlob,
+        ppErrorMsgs: *mut *mut ID3DBlob,
+    ) -> HRESULT;
+    pub fn D3DGetDebugInfo(
+        pSrcData: LPCVOID,
+        SrcDataSize: SIZE_T,
+        ppDebugInfo: *mut *mut ID3DBlob,
+    ) -> HRESULT;
+    pub fn D3DReflect(
+        pSrcData: LPCVOID,
+        SrcDataSize: SIZE_T,
+        pInterface: REFIID,
+        ppReflector: *mut *mut c_void,
+    ) -> HRESULT;
+    pub fn D3DReflectLibrary(
+        pSrcData: LPCVOID,
+        SrcDataSize: SIZE_T,
+        riid: REFIID,
+        ppReflector: *mut LPVOID,
+    ) -> HRESULT;
+}
 pub const D3D_DISASM_ENABLE_COLOR_CODE: DWORD = 0x00000001;
 pub const D3D_DISASM_ENABLE_DEFAULT_VALUE_PRINTS: DWORD = 0x00000002;
 pub const D3D_DISASM_ENABLE_INSTRUCTION_NUMBERING: DWORD = 0x00000004;
@@ -47,7 +132,64 @@ pub const D3D_DISASM_DISABLE_DEBUG_INFO: DWORD = 0x00000010;
 pub const D3D_DISASM_ENABLE_INSTRUCTION_OFFSET: DWORD = 0x00000020;
 pub const D3D_DISASM_INSTRUCTION_ONLY: DWORD = 0x00000040;
 pub const D3D_DISASM_PRINT_HEX_LITERALS: DWORD = 0x00000080;
+extern "system" {
+    pub fn D3DDisassemble(
+        pSrcData: LPCVOID,
+        SrcDataSize: SIZE_T,
+        Flags: UINT,
+        szComments: LPCSTR,
+        ppDisassembly: *mut *mut ID3DBlob,
+    ) -> HRESULT;
+    pub fn D3DDisassembleRegion(
+        pSrcData: LPCVOID,
+        SrcDataSize: SIZE_T,
+        Flags: UINT,
+        szComments: LPCSTR,
+        StartByteOffset: SIZE_T,
+        NumInsts: SIZE_T,
+        pFinishByteOffset: *mut SIZE_T,
+        ppDisassembly: *mut *mut ID3DBlob,
+    ) -> HRESULT;
+    pub fn D3DCreateLinker(
+        ppLinker: *mut *mut ID3D11Linker,
+    ) -> HRESULT;
+    pub fn D3DLoadModule(
+        pSrcData: LPCVOID,
+        cbSrcDataSize: SIZE_T,
+        ppModule: *mut *mut ID3D11Module,
+    ) -> HRESULT;
+    pub fn D3DCreateFunctionLinkingGraph(
+        uFlags: UINT,
+        ppFunctionLinkingGraph: *mut *mut ID3D11FunctionLinkingGraph,
+    ) -> HRESULT;
+}
 pub const D3D_GET_INST_OFFSETS_INCLUDE_NON_EXECUTABLE: DWORD = 0x00000001;
+extern "system" {
+    pub fn D3DGetTraceInstructionOffsets(
+        pSrcData: LPCVOID,
+        SrcDataSize: SIZE_T,
+        Flags: UINT,
+        StartInstIndex: SIZE_T,
+        NumInsts: SIZE_T,
+        pOffsets: *mut SIZE_T,
+        pTotalInsts: *mut SIZE_T,
+    ) -> HRESULT;
+    pub fn D3DGetInputSignatureBlob(
+        pSrcData: LPCVOID,
+        SrcDataSize: SIZE_T,
+        ppSignatureBlob: *mut *mut ID3DBlob,
+    ) -> HRESULT;
+    pub fn D3DGetOutputSignatureBlob(
+        pSrcData: LPCVOID,
+        SrcDataSize: SIZE_T,
+        ppSignatureBlob: *mut *mut ID3DBlob,
+    ) -> HRESULT;
+    pub fn D3DGetInputAndOutputSignatureBlob(
+        pSrcData: LPCVOID,
+        SrcDataSize: SIZE_T,
+        ppSignatureBlob: *mut *mut ID3DBlob,
+    ) -> HRESULT;
+}
 ENUM!{enum D3DCOMPILER_STRIP_FLAGS {
     D3DCOMPILER_STRIP_REFLECTION_DATA = 0x00000001,
     D3DCOMPILER_STRIP_DEBUG_INFO = 0x00000002,
@@ -56,6 +198,14 @@ ENUM!{enum D3DCOMPILER_STRIP_FLAGS {
     D3DCOMPILER_STRIP_ROOT_SIGNATURE = 0x00000010,
     D3DCOMPILER_STRIP_FORCE_DWORD = 0x7fffffff,
 }}
+extern "system" {
+    pub fn D3DStripShader(
+        pShaderBytecode: LPCVOID,
+        BytecodeLength: SIZE_T,
+        uStripFlags: UINT,
+        ppStrippedBlob: *mut *mut ID3DBlob,
+    ) -> HRESULT;
+}
 ENUM!{enum D3D_BLOB_PART {
     D3D_BLOB_INPUT_SIGNATURE_BLOB,
     D3D_BLOB_OUTPUT_SIGNATURE_BLOB,
@@ -74,7 +224,52 @@ ENUM!{enum D3D_BLOB_PART {
     D3D_BLOB_TEST_COMPILE_PERF,
     D3D_BLOB_TEST_COMPILE_REPORT,
 }}
+extern "system" {
+    pub fn D3DGetBlobPart(
+        pSrcData: LPCVOID,
+        SrcDataSize: SIZE_T,
+        Part: D3D_BLOB_PART,
+        Flags: UINT,
+        ppPart: *mut *mut ID3DBlob,
+    ) -> HRESULT;
+    pub fn D3DSetBlobPart(
+        pSrcData: LPCVOID,
+        SrcDataSize: SIZE_T,
+        Part: D3D_BLOB_PART,
+        Flags: UINT,
+        pPart: LPCVOID,
+        PartSize: SIZE_T,
+        ppNewShader: *mut *mut ID3DBlob,
+    ) -> HRESULT;
+    pub fn D3DCreateBlob(
+        Size: SIZE_T,
+        ppBlob: *mut *mut ID3DBlob,
+    ) -> HRESULT;
+}
 STRUCT!{struct D3D_SHADER_DATA {
     pBytecode: LPCVOID,
     BytecodeLength: SIZE_T,
 }}
+extern "system" {
+    pub fn D3DCompressShaders(
+        uNumShaders: UINT,
+        pShaderData: *mut D3D_SHADER_DATA,
+        uFlags: UINT,
+        ppCompressedData: *mut *mut ID3DBlob,
+    ) -> HRESULT;
+    pub fn D3DDecompressShaders(
+        pSrcData: LPCVOID,
+        SrcDataSize: SIZE_T,
+        uNumShaders: UINT,
+        uStartIndex: UINT,
+        pIndices: *mut UINT,
+        uFlags: UINT,
+        ppShaders: *mut *mut ID3DBlob,
+        pTotalShaders: *mut UINT,
+    ) -> HRESULT;
+    // pub fn D3DDisassemble10Effect(
+    //     pEffect: *mut ID3D10Effect,
+    //     Flags: UINT,
+    //     ppDisassembly: *mut *mut ID3DBlob,
+    // ) -> HRESULT;
+}
