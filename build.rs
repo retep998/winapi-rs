3b6082723344646c3df568f72dbf18075e4a684e
Bryant Mairs <bryant@mai.rs>
2017-08-14 09:59:25 -0700

    Migrate dinputd GUIDs to winapi 0.3

--- build.rs
+++ build.rs
@@ -24,6 +24,7 @@ const DATA: &'static [(&'static str, &'static [&'static str], &'static [&'static
     ("devguid", &[], &[]),
     ("devpkey", &["devpropdef"], &[]),
     ("devpropdef", &["guiddef", "minwindef", "winnt"], &[]),
+    ("dinputd", &[], &[]),
     ("dxgi", &["basetsd", "dxgiformat", "dxgitype", "guiddef", "minwindef", "unknwnbase", "windef", "winnt"], &["dxgi"]),
     ("dxgi1_2", &["basetsd", "dxgi", "dxgiformat", "dxgitype", "guiddef", "minwinbase", "minwindef", "unknwnbase", "windef", "winnt"], &[]),
     ("dxgi1_3", &["dxgi", "dxgi1_2", "dxgiformat", "guiddef", "minwindef", "unknwnbase", "windef", "winnt"], &["dxgi"]),
