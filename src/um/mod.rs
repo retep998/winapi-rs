77f758f123c8bf9a494155d99892a3d9e59cb930
Bryant Mairs <bryant@mai.rs>
2017-08-14 11:29:40 -0700

    Migrate d3d11_2 GUIDs to winapi 0.3

--- src/um/mod.rs
+++ src/um/mod.rs
@@ -36,6 +36,7 @@ pub mod gl;
 #[cfg(feature = "d3d10shader")] pub mod d3d10shader;
 #[cfg(feature = "d3d11")] pub mod d3d11;
 #[cfg(feature = "d3d11_1")] pub mod d3d11_1;
+#[cfg(feature = "d3d11_2")] pub mod d3d11_2;
 #[cfg(feature = "d3d11on12")] pub mod d3d11on12;
 #[cfg(feature = "d3d11sdklayers")] pub mod d3d11sdklayers;
 #[cfg(feature = "d3d11shader")] pub mod d3d11shader;
