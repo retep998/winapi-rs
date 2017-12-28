b9c05c3a67b65697fc8144a0999e955c414bb14c
Jason Newcomb <j.man487@gmail.com>
2017-12-28 08:42:35 -0500

    Complete wtypes

--- build.rs
+++ build.rs
@@ -73,7 +73,7 @@ const DATA: &'static [(&'static str, &'static [&'static str], &'static [&'static
     ("wnnc", &["minwindef"], &[]),
     ("ws2def", &["basetsd", "guiddef", "inaddr", "minwindef", "vcruntime", "winnt"], &[]),
     ("ws2ipdef", &["in6addr", "inaddr", "minwindef", "ws2def"], &[]),
-    ("wtypes", &["guiddef", "minwindef", "ntdef", "wtypesbase"], &[]),
+    ("wtypes", &["guiddef", "minwindef", "ntdef", "rpcndr", "wtypesbase"], &[]),
     ("wtypesbase", &["minwindef", "rpcndr", "winnt"], &[]),
     // ucrt
     // um
