51c10ce21ab6b2744fdddfb9069194c7aa0fd687
Jason Newcomb <j.man487@gmail.com>
2018-04-15 23:42:00 -0400

    Add wbemdisp

--- src/um/mod.rs
+++ src/um/mod.rs
@@ -207,6 +207,7 @@ pub mod gl;
 #[cfg(feature = "vsserror")] pub mod vsserror;
 #[cfg(feature = "vswriter")] pub mod vswriter;
 #[cfg(feature = "wbemcli")] pub mod wbemcli;
+#[cfg(feature = "wbemdisp")] pub mod wbemdisp;
 #[cfg(feature = "wbemprov")] pub mod wbemprov;
 #[cfg(feature = "wct")] pub mod wct;
 #[cfg(feature = "werapi")] pub mod werapi;
