884941971d9f0c24ec2c4e83b28f078db85bdce4
MSxDOS <15524350+msxdos@users.noreply.github.com>
2018-07-05 01:09:18 +0300

    Try complete uxtheme

--- build.rs
+++ build.rs
@@ -292,7 +292,7 @@ const DATA: &'static [(&'static str, &'static [&'static str], &'static [&'static
     ("userenv", &["minwindef", "winnt", "winreg"], &["userenv"]),
     ("usp10", &["minwindef", "ntdef", "windef", "winerror", "wingdi", "winnt"], &["usp10"]),
     ("utilapiset", &["minwindef", "ntdef"], &["kernel32"]),
-    ("uxtheme", &["minwindef", "windef", "winnt"], &["uxtheme"]),
+    ("uxtheme", &["commctrl", "minwindef", "windef", "wingdi", "winnt"], &["uxtheme"]),
     ("vsbackup", &["guiddef", "minwindef", "unknwnbase", "vss", "vswriter", "winnt", "wtypes"], &["vssapi"]),
     ("vss", &["guiddef", "minwindef", "unknwnbase", "winnt"], &[]),
     ("vsserror", &["winnt"], &[]),
