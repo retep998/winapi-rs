b723d56ac5101073fdd94390797af769cafe4896
Artúr Kovács <kovacs.artur.barnabas@gmail.com>
2018-09-19 18:32:14 +0200

    Add um::ole2::OleInitialize, shared::wtypes::DVASPECT (#651)
    * Add um::ole2::OleInitialize, shared::wtypes::tagDVASPECT
    
    This adds functionality required for IDropTarget.
    
    * Fixed previous commit
    
    Renamed tagDVASPECT to DVASPECT.
    
    Chaned use order in um::ole2
    
    Added "minwindef" dependecy for ole2 to the build script.
    
    * Fix for use ordering.

--- src/um/ole2.rs
+++ src/um/ole2.rs
@@ -4,10 +4,14 @@
 // <LICENSE-MIT or http://opensource.org/licenses/MIT>, at your option.
 // All files in the project carrying such notice may not be copied, modified, or distributed
 // except according to those terms.
+use shared::minwindef::LPVOID;
 use shared::windef::HWND;
 use um::oleidl::LPDROPTARGET;
 use um::winnt::HRESULT;
 extern "system" {
+    pub fn OleInitialize(
+        pvReserved: LPVOID
+    ) -> HRESULT;
     pub fn RegisterDragDrop(
         hwnd: HWND,
         pDropTarget: LPDROPTARGET,
