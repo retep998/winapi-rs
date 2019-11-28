e5a69d0ee5327f7596e27fa39dc7a01ef3a60935
TechPriest <yaminogakusei@gmail.com>
2019-11-28 10:27:46 +0200

    SLR_FLAGS, SLGP_FLAGS

--- src/um/shobjidl_core.rs
+++ src/um/shobjidl_core.rs
@@ -98,6 +98,29 @@ interface ITaskbarList2(ITaskbarList2Vtbl): ITaskbarList(ITaskbarListVtbl) {
     ) -> HRESULT,    
 }}
 
+ENUM!{enum SLR_FLAGS {
+    SLR_NONE	                    = 0,
+    SLR_NO_UI	                    = 0x1,
+    SLR_ANY_MATCH	                = 0x2,
+    SLR_UPDATE	                    = 0x4,
+    SLR_NOUPDATE	                = 0x8,
+    SLR_NOSEARCH	                = 0x10,
+    SLR_NOTRACK	                    = 0x20,
+    SLR_NOLINKINFO	                = 0x40,
+    SLR_INVOKE_MSI	                = 0x80,
+    SLR_NO_UI_WITH_MSG_PUMP	        = 0x101,
+    SLR_OFFER_DELETE_WITHOUT_FILE	= 0x200,
+    SLR_KNOWNFOLDER	                = 0x400,
+    SLR_MACHINE_IN_LOCAL_TARGET	    = 0x800,
+    SLR_UPDATE_MACHINE_AND_SID	    = 0x1000,
+    SLR_NO_OBJECT_ID	            = 0x2000,
+}}
+ENUM!{enum SLGP_FLAGS {
+    SLGP_SHORTPATH	                = 0x1,
+    SLGP_UNCPRIORITY	            = 0x2,
+    SLGP_RAWPATH	                = 0x4,
+    SLGP_RELATIVEPRIORITY	        = 0x8,
+}}
 DEFINE_GUID!{CLSID_ShellLink,
     0x0002140, 0x0000, 0x0000, 0xC0, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x46}
 RIDL!(#[uuid(0x000214EE, 0, 0, 0xC0,0,0,0,0,0,0,0x46)]
@@ -168,7 +191,6 @@ interface IShellLinkA(IShellLinkAVtbl): IUnknown(IUnknownVtbl) {
         pszFile: LPCSTR,
     ) -> HRESULT,
 });
-
 RIDL!(#[uuid(0x000214F9, 0, 0, 0xC0,0,0,0,0,0,0,0x46)]
 interface IShellLinkW(IShellLinkWVtbl): IUnknown(IUnknownVtbl) {
     fn GetPath(
