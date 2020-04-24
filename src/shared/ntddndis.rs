c0450b50e3a543b2e4b75bc6ebba0ad00e0b32e2
MSxDOS <15524350+MSxDOS@users.noreply.github.com>
2020-04-24 19:24:11 +0300

    Add Native Wifi / WLAN API support (#687)
    * Commit
    
    * Add parenthesis to NWF_DEFINE_OID
    
    * Remove © headers
    
    Co-authored-by: Peter Atashian <retep998@gmail.com>

--- src/shared/ntddndis.rs
+++ src/shared/ntddndis.rs
@@ -3,15 +3,24 @@
 // <LICENSE-MIT or http://opensource.org/licenses/MIT>, at your option.
 // All files in the project carrying such notice may not be copied, modified, or distributed
 // except according to those terms.
+use ctypes::c_int;
 use shared::ifdef::IF_MAX_STRING_SIZE;
-pub const NDIS_IF_MAX_STRING_SIZE: usize = IF_MAX_STRING_SIZE;
+use shared::minwindef::{UCHAR, USHORT};
+//290
+STRUCT!{struct NDIS_OBJECT_HEADER {
+    Type: UCHAR,
+    Revision: UCHAR,
+    Size: USHORT,
+}}
+pub type PNDIS_OBJECT_HEADER = *mut NDIS_OBJECT_HEADER;
+//2274
 ENUM!{enum NDIS_MEDIUM {
     NdisMedium802_3 = 0,
     NdisMedium802_5 = 1,
     NdisMediumFddi = 2,
     NdisMediumWan = 3,
     NdisMediumLocalTalk = 4,
-    NdisMediumDix = 5, // defined for convenience, not a real medium
+    NdisMediumDix = 5,
     NdisMediumArcnetRaw = 6,
     NdisMediumArcnet878_2 = 7,
     NdisMediumAtm = 8,
@@ -26,7 +35,7 @@ ENUM!{enum NDIS_MEDIUM {
     NdisMediumLoopback = 17,
     NdisMediumWiMAX = 18,
     NdisMediumIP = 19,
-    NdisMediumMax = 20, // Not a real medium, defined as an upper-bound
+    NdisMediumMax = 20,
 }}
 pub type PNDIS_MEDIUM = *mut NDIS_MEDIUM;
 ENUM!{enum NDIS_PHYSICAL_MEDIUM {
@@ -35,7 +44,7 @@ ENUM!{enum NDIS_PHYSICAL_MEDIUM {
     NdisPhysicalMediumCableModem = 2,
     NdisPhysicalMediumPhoneLine = 3,
     NdisPhysicalMediumPowerLine = 4,
-    NdisPhysicalMediumDSL = 5, // includes ADSL and UADSL (G.Lite)
+    NdisPhysicalMediumDSL = 5,
     NdisPhysicalMediumFibreChannel = 6,
     NdisPhysicalMedium1394 = 7,
     NdisPhysicalMediumWirelessWan = 8,
@@ -53,3 +62,14 @@ ENUM!{enum NDIS_PHYSICAL_MEDIUM {
     NdisPhysicalMediumMax = 20, // Not a real physical type, defined as an upper-bound
 }}
 pub type PNDIS_PHYSICAL_MEDIUM = *mut NDIS_PHYSICAL_MEDIUM;
+//2691
+pub type NDIS_STATUS = c_int;
+pub type PNDIS_STATUS = *mut c_int;
+//2736
+pub const NDIS_PACKET_TYPE_DIRECTED: u32 = 0x00000001;
+pub const NDIS_PACKET_TYPE_MULTICAST: u32 = 0x00000002;
+pub const NDIS_PACKET_TYPE_ALL_MULTICAST: u32 = 0x00000004;
+pub const NDIS_PACKET_TYPE_BROADCAST: u32 = 0x00000008;
+pub const NDIS_PACKET_TYPE_PROMISCUOUS: u32 = 0x00000020;
+//2835
+pub const NDIS_IF_MAX_STRING_SIZE: usize = IF_MAX_STRING_SIZE;
