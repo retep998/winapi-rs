d1ed5ffebc1fa3ebe22e070380001f3fa2e1175b
Yuval Deutscher <ha@deut.sh>
2021-01-23 16:05:32 +0200

    Add more info to the Interface trait

--- src/macros.rs
+++ src/macros.rs
@@ -174,7 +174,7 @@ macro_rules! RIDL {
         impl $interface {
             $(RIDL!{@method $(#[$($attrs)*])* fn $method($($p: $t,)*) -> $rtr})+
         }
-        RIDL!{@uuid $interface $($uuid),+}
+        RIDL!{@interface_trait $interface $vtbl $interface $($uuid),+}
     );
     (#[uuid($($uuid:expr),+)]
     interface $interface:ident ($vtbl:ident) : $pinterface:ident ($pvtbl:ident) {}) => (
@@ -184,7 +184,7 @@ macro_rules! RIDL {
             pub lpVtbl: *const $vtbl,
         }
         RIDL!{@deref $interface $pinterface}
-        RIDL!{@uuid $interface $($uuid),+}
+        RIDL!{@interface_trait $interface $vtbl $pinterface $($uuid),+}
     );
     (#[uuid($($uuid:expr),+)]
     interface $interface:ident ($vtbl:ident) : $pinterface:ident ($pvtbl:ident) {$(
@@ -201,7 +201,7 @@ macro_rules! RIDL {
             $(RIDL!{@method $(#[$($attrs)*])* fn $method($($p: $t,)*) -> $rtr})+
         }
         RIDL!{@deref $interface $pinterface}
-        RIDL!{@uuid $interface $($uuid),+}
+        RIDL!{@interface_trait $interface $vtbl $pinterface $($uuid),+}
     );
     (@deref $interface:ident $pinterface:ident) => (
         impl $crate::_core::ops::Deref for $interface {
@@ -259,20 +259,19 @@ macro_rules! RIDL {
             ) -> *mut $rtr,
         ) $($tail)*}
     );
-    (@uuid $interface:ident
+    (@interface_trait $interface:ident $vtbl:ident $pinterface:ident
         $l:expr, $w1:expr, $w2:expr,
         $b1:expr, $b2:expr, $b3:expr, $b4:expr, $b5:expr, $b6:expr, $b7:expr, $b8:expr
     ) => (
-        impl $crate::Interface for $interface {
-            #[inline]
-            fn uuidof() -> $crate::shared::guiddef::GUID {
-                $crate::shared::guiddef::GUID {
-                    Data1: $l,
-                    Data2: $w1,
-                    Data3: $w2,
-                    Data4: [$b1, $b2, $b3, $b4, $b5, $b6, $b7, $b8],
-                }
-            }
+        unsafe impl $crate::Interface for $interface {
+            type VTable = $vtbl;
+            type Super = $pinterface;
+            const IID: $crate::shared::guiddef::GUID = $crate::shared::guiddef::GUID {
+                Data1: $l,
+                Data2: $w1,
+                Data3: $w2,
+                Data4: [$b1, $b2, $b3, $b4, $b5, $b6, $b7, $b8],
+            };
         }
     );
     (@item $thing:item) => ($thing);
